927ba8b14f47429933f0ebda99d6e1cef94a997e
ChunYapBeep <127012228+ChunYapBeep@users.noreply.github.com>
2023-09-10 15:52:54 +0800

    add some table

---
+++ app/Models/DiseaseSymptom.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DiseaseSymptom extends Model
+{
+    use HasFactory;
+    protected $fillable = ['disease_id, symptom_id'];
+}
